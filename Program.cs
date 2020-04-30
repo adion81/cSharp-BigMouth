b4b9474b899814746cbf308b82ed0d3c624aaf34
Adrien Dion <adion81@gmail.com>
2020-04-30 07:52:14 -0500

    empty project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BigMouth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
